f232d9c0d1e516e211d4747fc64fb1a7c269c0ee
Matthis Kälble <69082876+matthis-k@users.noreply.github.com>
2026-09-01 20:53:04 +0200

    refactor(frontend): remove contract compatibility reexports

--- rust/crates/phenix-plugin-frontend/src/lib.rs
+++ rust/crates/phenix-plugin-frontend/src/lib.rs
@@ -1,9 +1,4 @@
 #![forbid(unsafe_code)]
-pub use phenix_sdk::{
-    ExecutionCommand, ExecutionInterface, ExecutionRecord, ExecutionResponse, ExecutionState,
-    FrontendCommand, FrontendInterface, FrontendProviderDescriptor, FrontendResponse,
-    FrontendServiceRequest, FrontendServiceResult, LiveFrontendProvider,
-};
 mod component;
 mod implementation;
 pub use component::*;
