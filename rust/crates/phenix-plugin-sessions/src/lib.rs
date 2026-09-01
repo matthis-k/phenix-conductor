d03110fb47d06b18e9f13000640a397ad1a45415
Matthis Kälble <69082876+matthis-k@users.noreply.github.com>
2026-09-01 20:54:29 +0200

    refactor(sessions): remove contract compatibility reexports

--- rust/crates/phenix-plugin-sessions/src/lib.rs
+++ rust/crates/phenix-plugin-sessions/src/lib.rs
@@ -3,7 +3,3 @@
 mod implementation;
 
 pub use implementation::*;
-pub use phenix_sdk::{
-    session_service, SessionCommand, SessionId, SessionInput, SessionInputKind, SessionInterface,
-    SessionRecord, SessionResponse, SESSION_SERVICE,
-};
